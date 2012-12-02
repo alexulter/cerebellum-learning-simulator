e035a3a9ec578da18234aee0d0cae67a8a0e4134
Alex Zhukov <alexulter@gmail.com>
2012-12-02 23:20:41 +0400

    added cerebellum learning (console)

---
+++ mauk_win/resource.h
@@ -0,0 +1,18 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by mauk_main.rc
+//
+#define IDD_MYDLG                       103
+#define IDR_MENU1                       107
+#define ID_FILE_EXIT                    40004
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        110
+#define _APS_NEXT_COMMAND_VALUE         40005
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           104
+#endif
+#endif
